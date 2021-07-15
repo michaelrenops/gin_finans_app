432108f16e1e6c558061efad29a53e8e6d56ba65
michaelrenops <renomichaelps@gmail.com>
2021-07-15 19:57:06 +0800

    sign up header, page view, and footer setup

---
+++ lib/pages/sign_up/views/sign_up_header.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignUpHeader extends StatelessWidget {
+  const SignUpHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
