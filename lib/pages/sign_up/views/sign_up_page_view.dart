38b662cb12df2d01a7cd4c78cb1be44125bb16d6
michaelrenops <renomichaelps@gmail.com>
2021-07-15 20:25:58 +0800

    added page view and page controller from sign up inherited widget in sign up page view

--- lib/pages/sign_up/views/sign_up_page_view.dart
+++ lib/pages/sign_up/views/sign_up_page_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:gin_finans_app/pages/inherited/sign_up_inherited.dart';
 
 class SignUpPageView extends StatelessWidget {
   const SignUpPageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SignUpInherited _signUpInherited = SignUpInherited.of(context);
+    PageController _pageController = _signUpInherited.pageController;
+
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+
+      ]
+    );
   }
 }
