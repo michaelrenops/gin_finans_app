d0684455ca4d2a2f73011a044325de930442af21
michaelrenops <renomichaelps@gmail.com>
2021-07-15 23:06:58 +0800

    added header, page view, and footer to sign up body

--- lib/pages/sign_up/views/sign_up_body.dart
+++ lib/pages/sign_up/views/sign_up_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gin_finans_app/pages/sign_up/views/sign_up_footer.dart';
+import 'package:gin_finans_app/pages/sign_up/views/sign_up_header.dart';
+import 'package:gin_finans_app/pages/sign_up/views/sign_up_page_view.dart';
 
 class SignUpBody extends StatelessWidget {
   const SignUpBody({Key? key}) : super(key: key);
@@ -8,7 +11,11 @@ class SignUpBody extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: <Widget>[
-
+          SignUpHeader(),
+          Expanded(
+            child: SignUpPageView(),
+          ),
+          SignUpFooter()
         ],
       )
     );
