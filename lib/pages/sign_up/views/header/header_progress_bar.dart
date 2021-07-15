c4d6d844e50684c862230c9ac08e3a6271f215e9
michaelrenops <renomichaelps@gmail.com>
2021-07-15 20:55:26 +0800

    added progress bar

--- lib/pages/sign_up/views/header/header_progress_bar.dart
+++ lib/pages/sign_up/views/header/header_progress_bar.dart
@@ -5,6 +5,27 @@ class HeaderProgressBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 10.0,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.all(Radius.circular(30.0))
+          ),
+        ),
+        AnimatedContainer(
+          width: (MediaQuery.of(context).size.width - 12.0) * 0.3,
+          height: 10.0,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(Radius.circular(30.0))
+          ),
+          duration: Duration(milliseconds: 250),
+          curve: Curves.linear
+        )
+      ],
+    );
   }
 }
