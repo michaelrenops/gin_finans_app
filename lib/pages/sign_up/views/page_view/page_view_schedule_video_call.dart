ab9e4930924f81da761592f03b6c1c66416f5a2e
michaelrenops <renomichaelps@gmail.com>
2021-07-15 22:51:31 +0800

    added animated icon

--- lib/pages/sign_up/views/page_view/page_view_schedule_video_call.dart
+++ lib/pages/sign_up/views/page_view/page_view_schedule_video_call.dart
@@ -38,6 +38,18 @@ class _PageViewScheduleVideoCallState extends State<PageViewScheduleVideoCall> w
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Container(
+              width: 54.0,
+              height: 54.0,
+              child: Center(
+                  child: AnimatedIcon(animation: animation)
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue.shade100,
+                shape: BoxShape.circle
+              ),
+            ),
+            SizedBox(height: 30.0),
             _title(),
             SizedBox(height: 20.0),
             _subTitle(),
@@ -62,6 +74,34 @@ class _PageViewScheduleVideoCallState extends State<PageViewScheduleVideoCall> w
       'Choose the date and time that you preferred, we will send you a link via email to make a video call on the scheduled date and time.',
       style: TextStyle(
         fontSize: 14.0
+      ),
+    );
+  }
+}
+
+class AnimatedIcon extends AnimatedWidget {
+  AnimatedIcon({
+    Key? key,
+    required Animation<double> animation
+  }) : super(key: key, listenable: animation);
+
+  final _iconSizeTween = Tween<double>(begin: 22.0, end: 28.0);
+  final _containerSizeTween = Tween<double>(begin: 42.0, end: 54.0);
+
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+
+    return Container(
+      width: _containerSizeTween.evaluate(animation),
+      height: _containerSizeTween.evaluate(animation),
+      child: Icon(
+        Icons.calendar_today,
+        color: Colors.white,
+        size: _iconSizeTween.evaluate(animation),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle
       ),
     );
   }
