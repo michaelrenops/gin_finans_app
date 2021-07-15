81f7162223e1d6305ccb35b4632f988c2bc680e3
michaelrenops <renomichaelps@gmail.com>
2021-07-16 00:39:28 +0800

    added route to home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => HomePage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
