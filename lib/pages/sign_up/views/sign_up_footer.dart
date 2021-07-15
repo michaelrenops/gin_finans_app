c11853a2202f649b86402934dbcbfd0df025fa47
michaelrenops <renomichaelps@gmail.com>
2021-07-15 20:12:15 +0800

    added next button in footer

--- lib/pages/sign_up/views/sign_up_footer.dart
+++ lib/pages/sign_up/views/sign_up_footer.dart
@@ -5,6 +5,34 @@ class SignUpFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(20.0),
+      child: TextButton(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text(
+            'Next',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          overlayColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColorDark),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)
+            )
+          )
+        ),
+        onPressed: () {
+
+        },
+      ),
+    );
   }
 }
